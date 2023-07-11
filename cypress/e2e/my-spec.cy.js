b3f804d5a9de05b70b5f3988f2f30bc5e820817a
Debora Carvalho Leite Wessen <debora14leite@gmail.com>
2023-07-11 19:53:22 -0400

    Feat: Test if the application adds the contact correctly

--- cypress/e2e/my-spec.cy.js
+++ cypress/e2e/my-spec.cy.js
@@ -9,5 +9,40 @@ describe('Test Contact App', () => {
     cy.get('table tbody tr').should('have.length', 1)
   })
 
-  // Add tests here
+  it('Test if the application adds the contact correctly.',()=>{
+
+    // Enter contact details
+    cy.get('div.form-row.justify-content-center>div:nth-child(1)>input').should('be.visible').type('Debora Wessen');
+    cy.get('div.form-row.justify-content-center>div:nth-child(2)>input').should('be.visible').type('111 222 3333');
+    cy.get('div.form-row.justify-content-center>div:nth-child(3)>input').should('be.visible').type('debora@test.com');
+
+    // Click the add button
+    cy.get("button[name='add']").should('be.visible').click();
+
+    // Validate if only one row has been added to the table
+    cy.get('table tbody tr').should('have.length', 2);
+
+    // Validate if the entered data matches the table
+    cy.get('table tbody>tr:nth-child(2)>td:nth-child(1)').should('have.text', 'Debora Wessen');
+    cy.get('table tbody>tr:nth-child(2)>td:nth-child(2)').should('have.text', '111 222 3333');
+    cy.get('table tbody>tr:nth-child(2)>td:nth-child(3)').should('have.text', 'debora@test.com');
+
+    const nameRegex = /^[a-zA-Z\s]+$/;
+    // Validate if the name matches the regex
+    cy.get('table tbody>tr:nth-child(2)>td:nth-child(1)').invoke('text').should('match', nameRegex);
+
+    const phoneRegex = /^(\+?1[-.\s]?)?\(?\d{3}\)?[-.\s]?\d{3}[-.\s]?\d{4}$/;
+    // Validate if the phone matches the regex
+    cy.get('table tbody>tr:nth-child(2)>td:nth-child(2)').invoke('text').should('match', phoneRegex);
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    // Validate if the email matches the regex
+    cy.get('table tbody>tr:nth-child(2)>td:nth-child(3)').invoke('text').should('match', emailRegex);
+
+    
+    // Validate if the table contains the data
+    cy.get('table tbody>tr:nth-child(2) td').should('contain', 'Debora Wessen');
+    cy.get('table tbody>tr:nth-child(2) td').should('contain', '111 222 3333');
+    cy.get('table tbody>tr:nth-child(2) td').should('contain', 'debora@test.com');
+  })
 });
